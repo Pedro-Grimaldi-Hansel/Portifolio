092a2abce839cbcbeb0cce06fe926acf25b64eb4
Pedro Grimaldi <pedroghansel@gmail.com>
2022-07-08 14:52:37 -0300

    resolvendo plano de fundo da home no mobile

--- index.php
+++ index.php
@@ -9,33 +9,34 @@
     <link rel="stylesheet" href="..\styles.css">
 </head>
 <body>
-    <?php require 'nav_bar.php'; ?>
-    <hr>
-	<div class="container-int">
+    <div class="body_curriculo">
+        <?php require 'nav_bar.php'; ?>
+        <hr>
+        <div class="container-int">
 
-            <div class="foto_de_perfil">
-				<img src="..\Pedro_perfil_400x400.jpg" 
-                alt="Foto de Pedro Grimaldi">
-            </div>
-            <div class="Titulo_de_introducao">
-			    <h1>Pedro <span>Grimaldi Hansel</span></h1>
-			    <h2>Estudante de Graduação em <a href="https://www2.ufjf.br/ufjf/ensino/graduacao/ciencias-exatas/" target="_blank"
-                >Ciencias Exatas</a> e <a href="https://www.ufjf.br/cursocomputacao/" target="_blank"
-                >Ciencias da Computação</a> (UFJF).</h2>
-				<h2>Projetista, acessor do departamento comercial e departamentos projetos na <a href="https://www.instagram.com/codejr/"target="_blank">Code Empresa Jr.</a></h2>
-			</div>
+                <div class="foto_de_perfil">
+                    <img src="..\Pedro_perfil_400x400.jpg" 
+                    alt="Foto de Pedro Grimaldi">
+                </div>
+                <div class="Titulo_de_introducao">
+                    <h1>Pedro <span>Grimaldi Hansel</span></h1>
+                    <h2>Estudante de Graduação em <a href="https://www2.ufjf.br/ufjf/ensino/graduacao/ciencias-exatas/" target="_blank"
+                    >Ciencias Exatas</a> e <a href="https://www.ufjf.br/cursocomputacao/" target="_blank"
+                    >Ciencias da Computação</a> (UFJF).</h2>
+                    <h2>Projetista, acessor do departamento comercial e departamentos projetos na <a href="https://www.instagram.com/codejr/"target="_blank">Code Empresa Jr.</a></h2>
+                </div>
+        </div>
+        
+        <div class="icons_container" style="display: inline_block"><br>
+            <img class="icons" alt="Pedro-Git" height="40" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg">
+            <img class="icons" alt="Pedro-C" height="40" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg">
+            <img class="icons" alt="Pedro-C++" height="40" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg">
+            <img class="icons" alt="Pedro-HTML" height="40" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg">
+            <img class="icons" alt="Pedro-CSS" height="40" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg">
+            <img class="icons" alt="Pedro-CSS" height="40" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg">
+            <img class="icons" alt="Pedro-PHP" height="40" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg">
+        </div>
     </div>
-    
-    <div class="icons_container" style="display: inline_block" align="center"><br>
-        <img class="icons" alt="Pedro-Git" height="40" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg">
-        <img class="icons" alt="Pedro-C" height="40" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg">
-        <img class="icons" alt="Pedro-C++" height="40" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg">
-        <img class="icons" alt="Pedro-HTML" height="40" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg">
-        <img class="icons" alt="Pedro-CSS" height="40" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg">
-        <img class="icons" alt="Pedro-CSS" height="40" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg">
-        <img class="icons" alt="Pedro-PHP" height="40" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg">
-    </div>
-
 
 </body>
 </html>
