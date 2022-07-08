0a7ff3e9a85afe05791b509160e48c7cfa2906ec
Pedro Grimaldi <pedroghansel@gmail.com>
2022-07-08 16:24:11 -0300

    ondas na navbar nao funcionram

--- nav_bar.php
+++ nav_bar.php
@@ -1,7 +1,7 @@
 <div class="nav_bar">
 
 	<div class="link_contato_home ">
-		<a href="..\index.php">Home</a></li>
+		<a href="..\index.php">Home</a>
 	</div>
 
 	<div class="link_contato">
@@ -10,7 +10,7 @@
 		<a href="..\paginas\contato.php">Contato</a>
 	</div>
 	<div class="link_navbar">
-		<a href="..\index.php">Home</a></li>
+		<a href="..\index.php">Home</a>
 		<a href="..\paginas\portifólio.php">Portifolio</a>
 		<a href="..\paginas\curriculo.php">Currículo</a>
 		<a href="..\paginas\contato.php">Contato</a>
